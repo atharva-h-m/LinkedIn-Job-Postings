71c2480697216b3c9f24c5bb971530ec905740aa
Atharva Malandkar <atharva.h.malandkar@gmail.com>
2024-11-18 10:56:38 +0530

    adding required files

---
+++ src/exception.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def error_message_detail(error, error_detail: sys):
+    exc_tb = error_detail.exc_info()[2]
+    error_message = "Error occured in python script named [{0}] at line number [{1}] with the following error message:\n[{2}]".format(
+        exc_tb.tb_frame.f_code.co_filename, exc_tb.tb_lineno, str(error)
+    )
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(
+            error_message, error_detail=error_detail
+        )
+
+    def __str__(self):
+        return self.error_message
